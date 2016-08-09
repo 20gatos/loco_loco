743219618833d469ca9fb2c7a2536eba26c9e690
Estudiante <Estudiante@FI454402-13.unal.loc>
2016-08-09 15:02:39 -0500

    mensaje

---
+++ src/NewClass.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author Sergio Romero
+ * @since 09/08
+ * 
+ */
+public class NewClass {
+    
+}
+
+class Shufflel { 
+    public static void main(String [] args) { 
+        
+        int x = 3;
+ if (x == 1){   
+    
+   System.out.print("d");
+   
+   x = x - 1;
+}
+
+ 
